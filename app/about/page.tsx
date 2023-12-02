fe552d0e1e877562c26a19b22695a0a3b2c11965
n1dddd <danielseniv@outlook.com>
2023-12-02 18:47:11 -0500

    Add pencil sketch of avenue storefront

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function About() {
-  return <div></div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <div style={{ position: "relative", width: "700px", height: "300px" }}>
+        <Image
+          src="/amr_sketch.png"
+          alt="Pencil Sketch of Avenue Marble Restoration on Dupont Ave."
+          fill={true}
+          className="flex mt-16"
+        />
+      </div>
+    </main>
+  );
 }
