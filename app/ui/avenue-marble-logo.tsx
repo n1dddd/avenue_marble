c7a803b5cfff31d89179860015b4d5e61a71d00c
n1dddd <danielseniv@outlook.com>
2023-12-02 18:49:17 -0500

    Change how logo renders at certain breakpoints

--- app/ui/avenue-marble-logo.tsx
+++ app/ui/avenue-marble-logo.tsx
@@ -3,13 +3,12 @@ import Link from "next/link";
 
 export default function AvenueLogo() {
   return (
-    <div className="flex justify-center">
+    <div className="flex xl:w-3/4 justify-center md:w-full mt-8">
       <Link href="/">
         <Image
           src="/logo.png"
-          width={1160}
-          height={210}
-          className="flex"
+          width={1920}
+          height={720}
           alt="Avenue Marble Restoration Logo"
         />
       </Link>
