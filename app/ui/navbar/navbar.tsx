3cc703d3661a85ea530282bd25e4140f45b28ecc
n1dddd <danielseniv@outlook.com>
2023-12-05 23:37:36 -0500

    Change styling to side navbar that takes entire page height

--- app/ui/navbar/navbar.tsx
+++ app/ui/navbar/navbar.tsx
@@ -2,7 +2,7 @@ import NavLinks from "./nav-links";
 
 export default function NavBar() {
   return (
-    <div className="flex flex-row grow xl:w-3/4 justify-around">
+    <div className="flex flex-row md:flex-col gap-y-48 sticky top-20">
       <NavLinks />
     </div>
   );
